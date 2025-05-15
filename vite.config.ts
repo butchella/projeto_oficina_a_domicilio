f6bab99b745eb30fca5e198db0e98632e9236815
butchella <italo.ant.mg@gmail.com>
2025-05-15 10:29:00 -0300

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+export default defineConfig({
+  base: "/projeto_oficina_a_domicilio/", // nome do repositório
+  plugins: [react()],
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
